b17c94e8a85ef58fd99aa4b0b993ea4cc21f97c1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-11-07 12:49:26 -0500

    ported simple UI to ICE8

--- ICE8A/SelectionForm.cs
+++ ICE8A/SelectionForm.cs
@@ -1,10 +1,44 @@
+using System;
+
 namespace ICE8A
 {
     public partial class SelectionForm : Form
     {
+        Random random = new Random();
+
         public SelectionForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method generates random values for the character attributes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Generate_Click(object sender, EventArgs e)
+        {
+            TextBox_AGL.Text = Roll5d10().ToString();
+            TextBox_STR.Text = Roll5d10().ToString();
+            TextBox_VGR.Text = Roll5d10().ToString();
+            TextBox_PER.Text = Roll5d10().ToString();
+            TextBox_INT.Text = Roll5d10().ToString();
+            TextBox_WIL.Text = Roll5d10().ToString();
+
+        }
+
+        /// <summary>
+        /// This method rolls 5d10 and returns the total
+        /// </summary>
+        /// <returns>An integer value between 5 and 50</returns>
+        int Roll5d10()
+        {
+            int total = 0;
+            for (int die = 0; die < 5; die++)
+            {
+                total += random.Next(1, 11);
+            }
+            return total;
         }
     }
 }
